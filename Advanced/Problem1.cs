c779bf1e1f79b343823507acb4f96246162d472c
ElitsaDim <89987372+ElitsaDim@users.noreply.github.com>
2021-09-15 23:24:58 +0300

    Add files via upload

---
+++ Advanced/Problem1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Retake18._08._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
