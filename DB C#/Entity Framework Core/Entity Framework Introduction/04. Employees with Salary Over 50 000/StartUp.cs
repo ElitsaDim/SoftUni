cdffd394213115051ca90d9708d053bc900ded9d
ElitsaDim <89987372+ElitsaDim@users.noreply.github.com>
2021-11-03 11:43:47 +0200

    Add files via upload

---
+++ DB C#/Entity Framework Core/Entity Framework Introduction/04. Employees with Salary Over 50 000/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext db = new SoftUniContext();
+            string result = GetEmployeesWithSalaryOver50000(db);
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var sb = new StringBuilder();
+
+            var employees = context
+                .Employees
+                .Where(e => e.Salary > 50000)
+                .OrderBy(e => e.FirstName)
+                .ToArray()
+                .Select(e => new { e.FirstName, e.Salary })
+                .ToArray();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} - {e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
